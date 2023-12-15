b1eec95c41796ed3f4e0843ba56f048f627250e1
thinksoch <153719168+thinksoch@users.noreply.github.com>
2023-12-15 16:24:12 +0530

    Add files via upload

---
+++ EnquiryFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnquiryForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnquiryFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
